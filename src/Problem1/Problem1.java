cdd005c01d7e0c2f65dd615e3a5b84cc9b9c4bcb
banuGabrielAndrei <banu.gabrielandrei@gmail.com>
2024-02-07 11:23:18 +0200

    weekly contest #92

--- src/Problem1/Problem1.java
+++ src/Problem1/Problem1.java
@@ -1,7 +1,30 @@
 package Problem1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Problem1 {
     public static void main(String[] args) {
-        System.out.println("prima problema");
+        var scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        scanner.close();
+        int[] v = new int[3];
+        v[0] = a;
+        v[1] = b;
+        v[2] = c;
+        Arrays.sort(v);
+        boolean isValid = true;
+        for (int i = 0; i < v.length - 1; ++i) {
+            if (v[i] != v[i + 1] - 1) {
+                isValid = false;
+            }
+        }
+        if (isValid) {
+            System.out.println("CONSECUTIVE");
+        } else {
+            System.out.println();
+        }
     }
 }
--- src/Problem2/Problem2.java
+++ src/Problem2/Problem2.java
@@ -1,7 +1,48 @@
 package Problem2;
 
-public class Problem2 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Problem2 {
+    private static final int MIN_TWO_DIGITS = 10;
+    private static final int MAX_TWO_DIGITS = 99;
+    private static final int MIN_THREE_DIGITS = 100;
+    private static final int MAX_THREE_DIGITS = 999;
+    private static final int MAX_LENGTH = 10000;
+
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        int[] fr = new int[MAX_LENGTH + 1];
+        int x = scanner.nextInt();
+        int sum = 0;
+        int counterTwoDigits = 0;
+        while (true) {
+            int number = scanner.nextInt();
+            if (number < 0) {
+                break;
+            }
+            if (isTwoDigits(number)) {
+                ++fr[number];
+            } else if (isThreeDigits(number)) {
+                if (number > x) {
+                    sum += number;
+                }
+            }
+        }
+        scanner.close();
+        for (int i = 0; i < MAX_LENGTH + 1; ++i) {
+            if (fr[i] > 0) {
+                ++counterTwoDigits;
+            }
+        }
+        System.out.println(counterTwoDigits);
+        System.out.println(sum);
+    }
+
+    public static boolean isTwoDigits(int nr) {
+        return nr >= MIN_TWO_DIGITS && nr <= MAX_TWO_DIGITS;
+    }
+
+    public static boolean isThreeDigits(int nr) {
+        return nr >= MIN_THREE_DIGITS && nr <= MAX_THREE_DIGITS;
     }
 }
