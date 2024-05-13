019a848904283d841944478b0136e13e557f3ce1
banuGabrielAndrei <banu.gabrielandrei@gmail.com>
2024-05-13 21:28:21 +0300

    ss

--- src/week106/Problem3.java
+++ src/week106/Problem3.java
@@ -2,8 +2,6 @@ package week106;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
